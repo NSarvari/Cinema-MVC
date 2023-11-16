07109bac6eb8caee0a190eb370be16ecedc2527f
Nabila Sarvari <nabilasarwary@abv.bg>
2023-11-16 13:42:52 +0200

    Initial-commit

---
+++ Cinema/Cinema/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = 
+                context.Movies.ToList();
+
+            return View(movies);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Movie movie)
+        {
+            context.Movies.Add(movie);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Cinema/Cinema/Data/ApplicationDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Cinema.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Define relationships between entities
+            modelBuilder.Entity<Movie>()
+                .HasOne(m => m.Genre)
+                .WithMany(g => g.Movies)
+                .HasForeignKey(m => m.GenreId);
+
+            modelBuilder.Entity<MovieActor>()
+                .HasKey(ma => new { ma.MovieId, ma.ActorId });
+
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(ma => ma.Movie)
+                .WithMany(m => m.MovieActors)
+                .HasForeignKey(ma => ma.MovieId);
+
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(ma => ma.Actor)
+                .WithMany(a => a.MovieActors)
+                .HasForeignKey(ma => ma.ActorId);
+
+            modelBuilder.Entity<Ticket>()
+                .HasOne(t => t.Movie)
+                .WithMany(m => m.Tickets)
+                .HasForeignKey(t => t.MovieId);
+
+            modelBuilder.Entity<Ticket>()
+                .HasOne(t => t.Visitor)
+                .WithMany(v => v.Tickets)
+                .HasForeignKey(t => t.VisitorId);
+
+            modelBuilder.Entity<Ticket>()
+                .HasOne(t => t.Place)
+                .WithMany(p => p.Tickets)
+                .HasForeignKey(t => t.PlaceId);
+        }
+    }
+}
