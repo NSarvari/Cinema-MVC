fb238d8712e0e38b799c01c588d43a632ffe0e2e
Nabila Sarvari <nabilasarwary@abv.bg>
2023-11-17 11:22:41 +0200

    GenreController-Added

---
+++ Cinema/Cinema/Controllers/GenreController.cs
@@ -0,0 +1,40 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var genres = context.Genres
+            .Include(m => m.Movies).ToList();
+
+            return View(genres);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Movies = context.Movies.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Genre genre)
+        {
+            context.Genres.Add(genre);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
