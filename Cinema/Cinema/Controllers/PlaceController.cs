e15749ffa015be74f3f53ef058f55ca8fa067225
Nabila Sarvari <nabilasarwary@abv.bg>
2023-11-23 09:51:34 +0200

    PlaceController_Added

---
+++ Cinema/Cinema/Controllers/PlaceController.cs
@@ -0,0 +1,70 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Controllers
+{
+    public class PlaceController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public PlaceController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var place = context.Places
+           .Include(t => t.Tickets)
+           .ToList();
+
+            return View(place);
+        }
+
+        //Add Place
+        public IActionResult Add()
+        {
+            ViewBag.Tickets = context.Tickets.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Place place)
+        {
+            context.Places.Add(place);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Update Place
+        public IActionResult Edit(int id)
+        {
+            var place = context.Places
+                .Include(m => m.Tickets)
+                .FirstOrDefault(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Tickets = context.Tickets.ToList();
+
+            return View(place);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Places.Update(place);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(place);
+        }
+    }
+}
